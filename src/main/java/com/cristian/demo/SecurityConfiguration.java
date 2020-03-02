e71ac3711cd999a46f2ff39f41b5b9f6f7cec047
GBTC440002UR <GBTC440002UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-02 12:14:03 -0500

    SpringBoot_401 corrected

--- src/main/java/com/cristian/demo/SecurityConfiguration.java
+++ src/main/java/com/cristian/demo/SecurityConfiguration.java
@@ -1,12 +1,15 @@
 package com.cristian.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -16,15 +19,36 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public static BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Autowired
+    private SSUserDetailsService userDetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return new SSUserDetailsService(userRepository);
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().anyRequest().authenticated()
-                .and().formLogin();
+                .authorizeRequests()
+                .antMatchers("/", "/h2-console/**", "/register").permitAll()
+                .antMatchers("/admin").access("hasAuthority('ADMIN')")
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout()
+                .logoutRequestMatcher( new AntPathRequestMatcher("/logout"))
+                .and()
+                .httpBasic();
+                http.csrf().disable();
+                http.headers().frameOptions().disable();
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication().withUser("user")
-        .password(passwordEncoder().encode("password")).authorities("USER");
+        auth.userDetailsService(userDetailsServiceBean())
+            .passwordEncoder(passwordEncoder());
+
     }
 }
